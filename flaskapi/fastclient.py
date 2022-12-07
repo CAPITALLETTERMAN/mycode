10551ae0486c060b9d3750b774daaf6732f9fd2b
CAPITALLETTERMAN <ky@gatheringhall.net>
2022-12-07 21:58:17 +0000

    So I started flaskin'

--- flaskapi/fastclient.py
+++ flaskapi/fastclient.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
 import requests
+import time
 
 REQUESTURL = "http://localhost:2224/fast"
 
 while True:
     req = requests.get(REQUESTURL)
+    time.sleep(10)
     if req.status_code != 200:
         break
 
